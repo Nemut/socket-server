d78c63749d16a3144fc4e9d09b9dc6e22f006b7b
Alexis Cabrera <alexis@proyectizate.com>
2019-09-21 10:55:07 +0200

    sección 4

--- classes/server.ts
+++ classes/server.ts
@@ -1,23 +1,61 @@
 import express from 'express';
 import { SERVER_PORT } from '../global/environment';
+import socketIO from 'socket.io';
+import http from 'http';
+import * as socket from '../sockets/sockets';
 
 
 export default class Server {
 
+    private static _instance: Server;
+
     public app: express.Application;
     public port: number;
 
+    public io: socketIO.Server;
+    private httpServer: http.Server;
 
-    constructor() {
+
+    private constructor() {
 
         this.app = express();
         this.port = SERVER_PORT;
+
+        // express trabaja sobre http
+        // necesitamos pasar la configuración del servidor a través de http
+        // para después pasarselo a socket io
+        this.httpServer = new http.Server( this.app );
+        this.io = socketIO( this.httpServer );
+
+        this.escucharSockets();
+
+    }
+
+    public static get instance() {
+        return this._instance || ( this._instance = new this() );
+    }
+
+    private escucharSockets() {
+
+        console.log('Escuchando conexiones - sockets');
+
+        this.io.on('connection', cliente => {
+
+            console.log('Cliente conectado');
+
+            // Mensajes
+            socket.mensaje( cliente, this.io );
+
+            // Desconectar
+            socket.desconectar( cliente );
+
+        });
 
     }
 
     start( callback: Function ) {
 
-        this.app.listen( this.port, callback() );
+        this.httpServer.listen( this.port, callback() );
         
     }
 
